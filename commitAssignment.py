1f942a371d7b4d99da8c34c2b0bf07ff95572a7d
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-07 10:17:45 +0700

    Add ignore, remove test, and complete commitAssignmen

--- commitAssignment.py
+++ commitAssignment.py
@@ -1,13 +1,24 @@
 import os
 import sys
+import zipfile
 from pathlib import Path
 
 commitFolder = sys.argv[1]
-fileList = os.listdir(commitFolder)
+pathToCommitFolder = Path(commitFolder)
+listDirectory = os.listdir(commitFolder)
+fileZipList = [f for f in listDirectory if f.endswith('.zip')]
 
-assert len(fileList) != 0, 'Folder is empty.'
-for filename in fileList:
-    os.system(f'git add {filename}')
-    os.system(f'git commit -m "Add {filename} for {commitFolder}"')
+assert len(fileZipList) != 0, 'Folder is empty.'
+
+for filename in fileZipList:
+    pathToFile = pathToCommitFolder / filename
+    with zipfile.ZipFile(pathToFile, 'r') as f:
+        f.extractall(commitFolder)
+
+listDirectory = os.listdir(commitFolder)
+commitFolderList = [f for f in listDirectory if not f.endswith('.zip')]
+for folderName in commitFolderList:
+    os.system(f'git add {pathToCommitFolder/folderName}')
+    os.system(f'git commit -m "Add {folderName} for {commitFolder}"')
 
 os.system('git push')
