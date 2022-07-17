1c51d31fc18690e10a8036c53c0a7edb1e1498de
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 01:05:26 +0700

    week 01 id 58 assignments

---
+++ Assignment-week01/week01_student58_TaQuangTung/week01_assignment02_student58_TaQuangTung.py
@@ -0,0 +1,11 @@
+'''
+Bài tập 2. VS Code
+a) VS Code là một phần mềm mã nguồn mở bởi vì đây là phần mềm mà code của chúng được công khai sử dụng, bạn có thể tùy chỉnh hay chia sẻ cho mọi người để cùng đóng góp chung
+
+b) Chỉ ra ít nhất hai text editor khác có chức năng tương tự VS Code: Pycharm, PyDev
+
+c) Giải thích ý nghĩa của meme sau
+Đây là 1 trong phần mềm mã nguồn mở dùng để code python cũng như nhiều ngôn ngữ lập trình khác
+
+'''
+
---
+++ Assignment-week01/week01_student58_TaQuangTung/week01_assignment03_student58_TaQuangTung.py
@@ -0,0 +1,11 @@
+'''
+Bài tập 3. pip
+a) Công cụ pip được sử dụng vào mục đích để tải các gói package hay thư viện của python 
+
+b) Giải thích ý nghĩa của câu lệnh pip freeze
+Là 1 trong những câu lệnh con của pip, có chức năng bỏ các gói và phiên bản của chúng để có đầu ra tiêu chuẩn
+
+c) Chỉ ra một công cụ khác có chức năng tương tự pip là easy install_
+
+'''
+
