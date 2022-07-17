9a7f1b77f3b7156489c5b9153a0388f22cdda912
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 01:01:29 +0700

    week 5 id 01 assignments

---
+++ Assignment-week01/week01_student51_NguyenThiThang/week01_assignment02_student51_NguyenThiThang.py
@@ -0,0 +1,3 @@
+print("a) VS Code la mot phan mem ma nguon mo, vi no duoc cong khai code, bat cu ai cung co quyen tai code ve va tien hanh chinh sua theo y minh")
+print("b) Sublime text, Atom")
+print("c) ")
---
+++ Assignment-week01/week01_student51_NguyenThiThang/week01_assignment03_student51_NguyenThiThang.py
@@ -0,0 +1,3 @@
+print("a) Cong cu pip duoc su dung voi muc dich cai dat va quan ly cac goi tieu chuan cho Python")
+print("b) pip freeze dung de hien thi cac package da duoc su dung trong du an")
+print("c) pipenv")
