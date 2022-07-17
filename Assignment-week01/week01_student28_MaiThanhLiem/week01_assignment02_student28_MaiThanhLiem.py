6c2043dec19b7ab7a5043def7d8819affb0dbdc4
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 01:05:25 +0700

    week 01 id 28 assignments

---
+++ Assignment-week01/week01_student28_MaiThanhLiem/week01_assignment02_student28_MaiThanhLiem.py
@@ -0,0 +1,9 @@
+# %%
+print('a) VS Code là một phần mềm mã nguồn mở, đúng hay sai? Vì sao?')
+print('>>>Sai. Tuy rằng VS Code là dự án được Microsoft phát triển như một phần mềm hoàn toàn là mã nguồn mở theo giấy phép của MIT, tuy nhiên các mã nhị phân được phân phối theo các giấy phép khồng miễn phí.')
+
+print('b) Chỉ ra ít nhất hai text editor khác có chức năng tương tự VS Code.')
+print('>>>Atom bởi GitHub, Eclipse bởi Eclipse Foundation.')
+
+print('c) Giải thích ý nghĩa của meme sau')
+print('>>>"Khi có người muốn tôi giới thiệu một phần mềm chỉnh sửa văn bản:", không phải VI - một phần mềm chỉnh sửa văn bản lỗi thời mà là Visual Studio Code.')
---
+++ Assignment-week01/week01_student28_MaiThanhLiem/week01_assignment03_student28_MaiThanhLiem.py
@@ -0,0 +1,9 @@
+# %%
+print('a) Công cụ pip được sử dụng vào mục đích gì?')
+print('>>>pip được sử dụng để cài đặt và quản lý các gói lệnh phần mềm được viết nên bởi Python.')
+
+print('b) Giải thích ý nghĩa của câu lệnh pip freeze.')
+print('>>>Liệt kê tên tất cả các gói lệnh và phiên bản của chúng đã được cài đặt.')
+
+print('c) Chỉ ra một công cụ khác có chức năng tương tự pip.')
+print('>>>pipenv.')
