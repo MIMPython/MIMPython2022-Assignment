e610022ce60c4ceda0a4a82b5cdb7cd8db87c8b2
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 01:05:25 +0700

    week 01 id 24 assignments

---
+++ Assignment-week01/week01_student24_NguyenKhanhHuyen/week01_assignment01_student24_NguyenKhanhHuyen.py
@@ -0,0 +1,7 @@
+phana = "a) Ngôn ngữ lập trình Python được tạo ra bởi Guido van Rossum và lần đầu ra mắt vào năm 1991."
+phanb = "b) Tên gọi Python được đặt tên như một sự tôn vinh dành cho đoàn hài kịch của Anh Monty Python. Tuy nhiên, Python cũng có nghĩa là con trăn, nên logo của ngôn ngữ này là hình con trăn"
+phanc = "c)Tính tới ngày 13/07/2022, phiên bản Python mới nhất được phát hành là phiên bản Python 3.10.5"
+print("Bài 1:")
+print(phana)
+print(phanb)
+print(phanc)
---
+++ Assignment-week01/week01_student24_NguyenKhanhHuyen/week01_assignment02_student24_NguyenKhanhHuyen.py
@@ -0,0 +1,7 @@
+phana= "a) VS Code là một phần mềm mã nguồn mở. D0 Microsoft cung cấp VS Code Github repo cho người dùng đóng góp cũng như tìm kiếm sự trợ giúp."
+phanb= "b) Các text editor khác có chức năng tương tự VS Code như Atom, Brackets, Notepad++,..."
+phanc= "c) Hình ảnh nói về việc đề xuất 1 trình soạn thảo văn bản. Khi thanh kiếm chưa mở hết, nó ghi Vi khiến chúng ta nghĩ là trình soạn thảo văn bản Vim nhưng khi mở ra hết thì thực chất nó là Visual Studio Code."
+print("Bài 2:")
+print(phana)
+print(phanb)
+print(phanc)
