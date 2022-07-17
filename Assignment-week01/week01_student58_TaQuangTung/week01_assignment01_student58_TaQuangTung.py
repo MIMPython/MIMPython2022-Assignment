6dc2e3d7505728a0fd5f4f5e3f2cf2fc70d93d2e
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 01:01:29 +0700

    week 5 id 01 assignments

---
+++ Assignment-week01/week01_student58_TaQuangTung/week01_assignment01_student58_TaQuangTung.py
@@ -0,0 +1,10 @@
+'''
+Bài tập 1. Python
+a) Ngôn ngữ lập trình Python được tạo ra bởi Guido van Rossumvà vào năm 1991
+
+b) Vì sao ngôn ngữ lập trình Python lại được đặt tên như vậy?
+Do Rossum lấy cảm hứng từ chương trình hài của một phần trong sê-ri “Monty Python’s Flying Circus”
+
+c) Tính tới ngày 13/07/2022, phiên bản Python mới nhất được phát hành là phiên bản Python 3.9.1
+'''
+
