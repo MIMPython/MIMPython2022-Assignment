60c3f7445d158e03f96b4132af064503824573be
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 01:01:29 +0700

    week 2 id 01 assignments

---
+++ Assignment-week01/week01_student28_MaiThanhLiem/week01_assignment01_student28_MaiThanhLiem.py
@@ -0,0 +1,9 @@
+# %%
+print('a) Ngôn ngữ lập trình Python được tạo ra bởi ai và vào thời gian nào?')
+print('>>>Guido van Rossum trong khoảng những năm 1980.')
+
+print('b) Vì sao ngôn ngữ lập trình Python lại được đặt tên như vậy?')
+print('>>>Guido van Rossum cần một cái tên ngắn, độc và một chút gì đó bí ẩn. Và trong khi tạo ra Python, ông đang đọc kịch bản của bộ phim hài "Monty Python/s Flying Circus".')
+
+print('c) Tính tới ngày 13/07/2022, phiên bản Python mới nhất được phát hành là phiên bản nào?')
+print('>>>Phiên bản mới nhất của Python tính tới thời điểm hiện tại là 3.10.5.')
