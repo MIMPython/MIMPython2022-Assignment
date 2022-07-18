3ad35a35868dda9c987ccb96ebb3ae24c25a0060
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 20:13:19 +0700

    week 01 id 27 assignments

---
+++ Assignment-week01/week01_student27_NguyenDanLe/week01_assignment01_student27_NguyenDanLe.py
@@ -0,0 +1,3 @@
+print("a) Python được tạo ra bởi Guido Van Rossum vào tháng 2 năm 1991")
+print("b) Python được lấy cảm hứng từ chương trình của nhóm hài Monty Python")
+print("c) Tính đến ngày 17/7, phiên bản mới nhất của Python là 3.10.5")
---
+++ Assignment-week01/week01_student27_NguyenDanLe/week01_assignment02_student27_NguyenDanLe.py
@@ -0,0 +1,3 @@
+print("a) Visual studio code là phần mềm mã nguồn mở vì ta có thể dùng, tải xuống miễn phí, chỉnh sửa, tùy biến phần mềm")
+print("b) Một số text editor có thể thay thế VS CODE là Sublime text, Atom")
+print("c) VI vừa là hai chữ cái đầu tiên của VISUAL STUDIO CODE vừa là một text editor khác. Nếu người nghe không nghe hết câu thì có thể sẽ tưởng tác giả khuyên dùng VI")
