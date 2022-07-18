0c9a2cb96777dcab93c80d04731be9a11548fec4
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 20:13:18 +0700

    week 01 id 22 assignments

---
+++ Assignment-week01/week01_student22_VuTrinhHoang/week01_assignment01_student22_VuTrinhHoang.py
@@ -0,0 +1,28 @@
+# câu 1: 
+#     a) Python được tạo ra bởi Guido van Rossum vào năm 1991
+#     b) Những năm 1970 tại Anh, Đài BBC có cho phát sóng các tập phim của đoàn hài kịch siêu thực Monty Python. 
+# Một nụ cười bằng mười thang thuốc bổ, cộng thêm những năm 70 Anh Quốc vẫn đang trong quá trình phát triển 
+# kinh tế, Monty Python nhanh chóng trở nên phổ biến và trở thành một trong những show truyền hình được 
+# yêu thích nhất trong các quốc gia nói tiếng Anh thời bấy giờ. Rossum hâm mộ chương trình này rất nhiều, 
+# và anh đã đặt tên cho “đứa con đầu lòng” của mình là Python, với hy vọng đứa con lớn lên có thể trở nên 
+# nổi tiếng và được người dân ưa chuộng.
+#     c) Tính tới ngày 13/07/2022, phiên bản hoàn thiện nhất của Python mới nhất và được phát hành là phiên bản 3.10.4
+# Câu 2:
+#     a) VS Code có là một phần mềm mã nguồn mở, vì VSCode cung cấp cung cấp bất cứ người dùng quyền được sử dụng,
+# sao chép, thay đổi và chỉnh sửa phần mềm mà không bị giới hạn
+#     b) Một số text editor có chức năng tương tự VSCode:
+#         + Eclipse
+#         + NetBeans
+#     c) Ý nghĩa của meme: Trong meme có xuất hiện 2 cụm từ: "VI" và "Visual Studio Code".
+#         +"VI" (viết tắt của "Visual") là một trình soạn thảo văn bản rất phổ biến trong Linux, 
+# mỗi hành động được thực hiện bằng một phím trên bàn phím hoặc các lệnh chuyên dụng. 
+#         +"Visual Studio Code" là một trình soạn thảo mã nguồn mở, có giao diện đẹp và người dùng 
+# dễ dàng tương tác và sử dụng hơn so với "VI"
+#         =>Như vậy, meme này nhằm đề xuất sử dụng "Visual Studio Code" hơn là sử dụng "VI".
+# Câu 3: 
+#     a) pip là một trình quản lý thư viện cho Python, viết tắt của từ Preferred Installer Program. 
+# Đây là một tiện ích dòng lệnh cho phép bạn cài đặt, cài đặt lại hoặc gỡ cài đặt các gói PyPI bằng 
+# một dòng lệnh đơn giản và dễ hiểu
+#     b) pip freeze dùng để liệt kê và lưu tất cả các package cùng phiên bản hiện có trong môi trường 
+# bạn đang làm việc.
+#     c) Một công cụ khác có chức năng tương tự pip: PM (Pakages Manager)
---
+++ Assignment-week01/week01_student22_VuTrinhHoang/week01_assignment02_student22_VuTrinhHoang.py
@@ -0,0 +1,12 @@
+# Câu 2:
+#     a) VS Code có là một phần mềm mã nguồn mở, vì VSCode cung cấp cung cấp bất cứ người dùng quyền được sử dụng,
+# sao chép, thay đổi và chỉnh sửa phần mềm mà không bị giới hạn
+#     b) Một số text editor có chức năng tương tự VSCode:
+#         + Eclipse
+#         + NetBeans
+#     c) Ý nghĩa của meme: Trong meme có xuất hiện 2 cụm từ: "VI" và "Visual Studio Code".
+#         +"VI" (viết tắt của "Visual") là một trình soạn thảo văn bản rất phổ biến trong Linux, 
+# mỗi hành động được thực hiện bằng một phím trên bàn phím hoặc các lệnh chuyên dụng. 
+#         +"Visual Studio Code" là một trình soạn thảo mã nguồn mở, có giao diện đẹp và người dùng 
+# dễ dàng tương tác và sử dụng hơn so với "VI"
+#         =>Như vậy, meme này nhằm đề xuất sử dụng "Visual Studio Code" hơn là sử dụng "VI".
