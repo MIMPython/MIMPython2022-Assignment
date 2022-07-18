f1b790925aa74c399e0edeafa7ead0246e3d73f5
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 20:13:17 +0700

    week 01 id 08 assignments

---
+++ Assignment-week01/week01_student08_NguyenVanDai/week01_assignment01_student08_NguyenVanDai.py
@@ -0,0 +1,5 @@
+# Bài 1 
+# # Python (phát âm tiếng Anh: /ˈpaɪθɑːn/) là một ngôn ngữ lập trình bậc cao cho các mục đích lập trình đa năng,
+# # do Guido van Rossum tạo ra và lần đầu ra mắt vào năm 1991.
+# Tên gọi Python được đặt tên theo đoàn hài kịch của Anh "Monty Python"
+# Tính tới ngày 13/07/2022, phiên bản Python mới nhất được phát hành là phiên bản python 3.10.5
---
+++ Assignment-week01/week01_student08_NguyenVanDai/week01_assignment03_student08_NguyenVanDai.py
@@ -0,0 +1,4 @@
+# Bài 3 
+# PIP là một trình quản lý thư viện cho Python, viết tắt của từ Preferred Installer Program. Đây là một tiện ích dòng lệnh cho phép bạn cài đặt, cài đặt lại hoặc gỡ cài đặt các gói PyPI bằng một dòng lệnh đơn giản và dễ hiểu
+# pip freeze là một lệnh rất hữu ích, vì nó cho bạn biết bạn đã cài đặt những mô-đun nào với cài đặt pip và phiên bản của những mô-đun này mà bạn hiện đã cài đặt trên máy tính của mình
+# một công cụ khác có chức năng tương tự pip là git 
