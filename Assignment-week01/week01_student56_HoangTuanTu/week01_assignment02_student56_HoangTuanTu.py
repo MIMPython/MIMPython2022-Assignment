0c6baa6797fc2b2fd3f2954aeca02a8be71802c5
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 20:13:21 +0700

    week 01 id 56 assignments

---
+++ Assignment-week01/week01_student56_HoangTuanTu/week01_assignment02_student56_HoangTuanTu.py
@@ -0,0 +1,41 @@
+"""
+BT1
+a) Ngôn ngữ lập trình Python được tạo ra bởi ai và vào thời gian nào?
+-> Trả lời: Ngôn ngữ lập trình Python được tạo ra bởi tạo ra vào cuối những năm 1980 
+bởi lập trình viên người Hà Lan Guido van Rossum dựa trên nền tảng ngôn ngữ lập trình ABC. 
+___________________________________________________________________________________________________
+
+b) Vì sao ngôn ngữ lập trình Python lại được đặt tên như vậy?
+-> Trả lời: Tác giả coi đây chỉ như là một sở thích trong lúc rảnh rỗi của kỳ nghỉ Giáng sinh. Và 
+đến thời điểm để phổ biến ngôn ngữ này thì Van Rossum muốn có tên gọi sáng tạo dựa trên các tiêu 
+chí như ngắn, độc đáo và một chút bí ẩn. Ông đã tìm thấy nguồn cảm hứng khi xem chương trình của 
+nhóm hài nổi tiếng người Anh: Monty Python.
+
+c) Tính tới ngày 13/07/2022, phiên bản Python mới nhất được phát hành là phiên bản nào?
+-> Trả lời: Phiên bản mới nhất là python 3.10.5 được phát hành vào ngày 06/06/2022
+___________________________________________________________________________________________________
+
+BT2
+a) VS Code là một phần mềm mã nguồn mở, đúng hay sai? Vì sao?
+-> Trả lời: VS Code là một phần mềm mã nguồn mở vì bất cứ ai cũng có thể dùng miễn phí, tải xuống, 
+và dử dụng.
+
+b) Chỉ ra ít nhất hai text editor khác có chức năng tương tự VS Code.
+-> Trả lời: Sublime và Atom
+
+c) Giải thích ý nghĩa của meme sau
+-> Trả lời: VS Code tốt hơn so với VI ( một text eidtor khác chạy trên hệ máy linux)
+___________________________________________________________________________________________________
+
+BT3
+a) Công cụ pip được sử dụng vào mục đích gì?
+-> Trả lời: pip là một trình quản lý thư viện cho Python, viết tắt của từ Preferred Installer Program. 
+Đây là một tiện ích dòng lệnh cho phép bạn cài đặt, cài đặt lại hoặc gỡ cài đặt các gói PyPI bằng 
+một dòng lệnh đơn giản và dễ hiểu.
+
+b) Giải thích ý nghĩa của câu lệnh pip freeze.
+-> Trả lời: Hiện thị tất cả các thư viện đã cài đặt.
+
+c) Chỉ ra một công cụ khác có chức năng tương tự pip.
+-> Trả lời: easy_install
+"""
