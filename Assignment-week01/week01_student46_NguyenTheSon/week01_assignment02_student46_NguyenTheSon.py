cf430c1ecf2b81e63da65bb16bd3ecf99298eda9
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 20:13:20 +0700

    week 01 id 46 assignments

---
+++ Assignment-week01/week01_student46_NguyenTheSon/week01_assignment02_student46_NguyenTheSon.py
@@ -0,0 +1,12 @@
+# %%
+Bài 2:
+a)VS code là một phần mềm mã nguồn mở. Vì VS code là một biên tập lập trình code cho phép người dùng thay đổi theme, phím tắt,
+và nhiều chức năng khác
+
+b)Các text editor có chức năng tương tự VS code như là Atom, Notepad++, Sublime Text,...
+
+c)Khi ai đó bảo tôi giới thiệu về 1 cái text editor thì họ tưởng tôi sẽ giới thiệu VI (1 text editor cơ bản ) 
+nhưng không đó là VISUAL STUDIO CODE(1 text editor rất nhanh, nhẹ và cực kì mạnh)
+
+
+
---
+++ Assignment-week01/week01_student46_NguyenTheSon/week01_assignment03_student46_NguyenTheSon.py
@@ -0,0 +1,11 @@
+# %%
+Bài 3:
+a) pip là một trình quản lý thư viện cho Python, viết tắt của từ Preferred Installer Program.
+Đây là một tiện ích dòng lệnh cho phép bạn cài đặt, cài đặt lại hoặc gỡ cài đặt các gói PyPI bằng một dòng lệnh đơn giản và dễ hiểu.
+
+b)Câu lệnh pip freeze sẽ lưu lại tất cả các package cùng phiên bản hiện có trong môi trường trong một file có dạng requirements.txt 
+
+c)Công cụ có chức năng giống với pip: conda.Nó là một ackage manager và enviroment manager cũng có chức năng cài đặt các gói package.
+
+
+
