5963e8d89938641e8f9e4f166e1f66297b5c4080
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-07-18 20:13:21 +0700

    week 01 id 55 assignments

---
+++ Assignment-week01/week01_student55_NguyenThanhTrung/week01_assignment03_student55_NguyenThanhTrung.py
@@ -0,0 +1,7 @@
+'''
+a, pip là 1 tiện ích dòng lệnh cho phép ta cài đặt và quản lý các thư viện bổ sung không nằm trong gói thư viện chuẩn khi cài python
+b, khi ta chia sẻ dự án python của mình với mọi người ta nên hướng đến việc mọi người dùng cùng phiên bản của các gói bên ngoài mà ta 
+sử dụng (do phiên bản bạn sử dụng có chứa 1 tính năng mới hoặc phiên bản bạn dùng không tương thích với các phiên bản cũ), lệnh pip freeze 
+giúp ta giải quyết vấn đề này bằng cách xuất ra chính xác các gói cần được cài đặt và các phiên bản tương ứng
+c, (em không chắc chắn về câu trả lời phần này nên em xin phép để trống) 
+'''
