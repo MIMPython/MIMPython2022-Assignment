ece2af284fbba0d93bdeeef41d4c9cacf30bf7b2
Tran Thanh Tung <thanhtung1052001@gmail.com>
2022-08-15 00:15:11 +0700

    week 05 id 05 assignments

---
+++ Assignment-week05/week05_student05_LeThiMinhAnh/week05_assignment03_student05_LeThiMinhAnh.py
@@ -0,0 +1,16 @@
+'''
+OS: Windows
+
+cd x
+mkdir foo
+cd foo
+code data.txt
+cp data.txt newData.txt   
+cp data.txt newData_2.txt 
+cd ..
+mkdir bar
+cd foo
+mv newData.txt ~/Desktop/x/bar        
+del newData_2.txt
+
+'''
